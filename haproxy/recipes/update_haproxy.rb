bb5297f84646980c10ded9865b917999b6786cef
Sumit Murari <smurari@tavisca.com>
2015-10-29 10:47:26 +0530

    install updated haproxy

---
+++ haproxy/recipes/update_haproxy.rb
@@ -0,0 +1,16 @@
+
+bash 'GetUpdatedHaproxyextract_module' do
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+    add-apt-repository ppa:vbernat/haproxy-1.6
+    apt-get update
+    apt-get install haproxy
+    EOH
+end
+
+file '/var/www/customers/public_html/index.php' do
+  content '<html>This is a placeholder for the home page.</html>'
+  mode '0755'
+  owner 'web_admin'
+  group 'web_admin'
+end
